77f1ee767a1e47668699aa97e23d28c6ce5831fb
TangWei <854329260@qq.com>
2021-01-21 15:11:30 +0800

    重置查询参数

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * @param {Object} searchParams 表单查询参数
+ */
+export function resetSearchParams(searchParams) {
+  const resetParams = {};
+  for (let key in searchParams) {
+    resetParams[key] = undefined;
+  }
+  return resetParams;
+}
